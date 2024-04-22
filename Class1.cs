d18a3b9c3faadb49bf9ad84a5d4b799f44b1f00f
DaveCorleto <davide.corleto@fastwebnet.it>
2024-04-22 18:31:58 +0200

    Istanziamenti nel Main e classi corrette ma ancora incomplete

---
+++ Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Biblioteca
+    {
+
+        public List<Utente> Utenti { get; } = new List<Utente>();
+        public void AggiungiUtente(Utente utente)
+        {
+            Utenti.Add(utente);
+        }
+        // Costruttore vuoto di Biblioteca da richiamare in tutte le classi figlie
+        public Biblioteca() { }
+    }
+
+    //public class Prestito : Biblioteca {
+
+    //    public DateTime inizioPrestito;
+    //    public DateTime finePrestito;
+    //    private Utente beneficiario;
+    //    private Documenti documentoPrestato;
+       
+
+    //    public Utente Beneficiario
+    //    {
+    //        get { return beneficiario; } 
+    //        set { beneficiario = value; }
+    //    }
+
+    //    public Documenti DocumentoPrestato
+    //    {
+    //        get { return documentoPrestato; }
+    //        set { documentoPrestato = value; }
+    //    }
+
+
+    //    public Prestito() :base(){ 
+    //        this.inizioPrestito = DateTime.Now;
+    //        this.finePrestito = inizioPrestito.AddDays(3);
+
+    //    }
+    //}
+
+
+    public class Utente
+    {
+        public string cognome;
+        public string nome;
+        public string email;
+        public string password;
+        public string recapitoTelefonico;
+        public Utente(string cognome, string nome, string email, string password, string recapitoTelefonico) : base()
+             // Chiamata al costruttore senza parametri della classe base
+        {
+            this.cognome = cognome;
+            this.nome = nome;
+            this.email = email;
+            this.password = password;
+            this.recapitoTelefonico = recapitoTelefonico;
+        }
+        //metodo prendi in prestito documento (Libro o dvd)
+    }
+
+    public class Documenti
+    {
+        public int ISBN;
+        public string titolo;
+        public string anno;
+        public string settore;
+        public string scaffale;
+        public string autore; 
+
+        public Documenti(int ISBN, string titolo, string anno, string settore, string scaffale, string autore) :base(){
+
+            Random code = new Random();   
+
+            this.ISBN = code.Next(100000,999999);
+            this.titolo = titolo;
+            this.anno = anno;
+            this.settore = settore;
+            this.scaffale = scaffale;
+            this.autore = autore;
+        }
+
+    }
+
+    public class Dvd : Documenti
+    {
+        public int durata;
+
+        public Dvd(int ISBN, string titolo, string anno, string settore, string scaffale, string autore, int durata)
+        : base(ISBN, titolo, anno, settore, scaffale, autore)
+        {
+            this.durata = durata;
+        }
+
+    }
+    public class Libro : Documenti
+    {
+        public int numeroPagine;
+        public Libro (int ISBN, string titolo, string anno, string settore, string scaffale, string autore, int numeroPagine)
+        : base(ISBN, titolo, anno, settore, scaffale, autore)
+        {
+            this.numeroPagine = numeroPagine;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Creazione Utenti
+
+            Utente JohnTravolta = new Utente("Travolta", "John", "john.travolta@example.com", "password123", "3912356789");
+            Utente MarioRossi = new Utente("Rossi", "Mario", "mario.rossi@example.com", "password123", "3912356789");
+            Utente LauraBianchi = new Utente("Bianchi", "Laura", "laura.bianchi@example.com", "qwerty456", "39987654321");
+            Utente SarahSmith = new Utente("Smith", "Sarah", "sarah.smith@example.com", "sarah123", "447890123456");
+            Utente MarcoGialli = new Utente("Gialli", "Marco", "marco.gialli@example.com", "marco456", "39333987654");
+            Utente AnnaJohnson = new Utente("Johnson", "Anna", "anna.johnson@example.com", "anna789", "441234567890");
+            Utente EmmaWhite = new Utente("White", "Emma", "emma.white@example.com", "emma345", "393339876543");
+            Utente LucaVerdi = new Utente("Verdi", "Luca", "luca.verdi@example.com", "luca678", "393383838383");
+            Utente SofiaBrown = new Utente("Brown", "Sofia", "sofia.brown@example.com", "sofia123", "441234567890");
+            Utente MichaelGray = new Utente("Gray", "Michael", "michael.gray@example.com", "michael456", "447890123456");
+
+            //Creazione Libri 
+
+            Libro ilNomeDelVento = new Libro(1234567890, "Il nome del vento", "2007", "Fantasy", "A3", "Patrick Rothfuss", 662);
+            Libro milleSplendidiSoli = new Libro(46546579, "Mille Splendidi Soli", "2002", "Tragico", "B1", "Khaled Hosseini", 450);
+            Libro ilSignoreDegliAnelli = new Libro(97888452, "Il Signore degli Anelli", "1954", "Fantasy", "A2", "J.R.R. Tolkien", 1216);
+            Libro harryPotter = new Libro(97832743, "Harry Potter e la Pietra Filosofale", "1997", "Fantasy", "A1", "J.K. Rowling", 336);
+            Libro cronicheDelGhiaccioEldu = new Libro(97843433, "Le Cronache del Ghiaccio e del Fuoco", "1996", "Fantasy", "C2", "George R.R. Martin", 547);
+            Libro ilCodiceDaVinci = new Libro(978140179, "Il Codice da Vinci", "2003", "Thriller", "D1", "Dan Brown", 592);
+            Libro orgoglioEPregiudizio = new Libro(978019518, "Orgoglio e Pregiudizio", "1813", "Romanzo", "B3", "Jane Austen", 432);
+            Libro centoAnniDiSolitudine = new Libro(97880419, "Cento anni di solitudine", "1967", "Romanzo", "C1", "Gabriel García Márquez", 432);
+
+            //Creazione Dvd 
+
+            Dvd titanic = new Dvd(616546126, "Titanic", "1997", "Classico", "A6", "Non me lo ricordo", 150);
+            Dvd inception = new Dvd(50578841, "Inception", "2010", "Thriller", "B4", "Christopher Nolan", 148);
+            Dvd loHobbit = new Dvd(50546959, "Lo Hobbit", "2001", "Fantasy", "A2", "Peter Jackson", 178);
+            Dvd matrix = new Dvd(73219443, "Matrix", "1999", "Sci-Fi", "B1", "Lana Wachowski", 136);
+            Dvd forrestGump = new Dvd(505092714, "Forrest Gump", "1994", "Drammatico", "C3", "Robert Zemeckis", 142);
+            Dvd ilPianista = new Dvd(505058220, "Il Pianista", "2002", "Drammatico", "D2", "Roman Polanski", 150);
+            Dvd fightClub = new Dvd(80235089, "Fight Club", "1999", "Drammatico", "C1", "David Fincher", 139);
+            Dvd pulpFiction = new Dvd(871741912, "Pulp Fiction", "1994", "Crime", "B2", "Quentin Tarantino", 154);
+            Dvd schindlersList = new Dvd(505004632, "Schindler's List", "1993", "Drammatico", "D3", "Steven Spielberg", 195);
+            Dvd theShawshankRedemption = new Dvd(732191869, "The Shawshank Redemption", "1994", "Drammatico", "A5", "Frank Darabont", 142);
+
+
         }
     }
 }
